a14818e87fbcc6517842c4135b4dc0d35c229558
Artsiom Murashko <ksarelto@tut.by>
2021-11-12 18:22:07 +0300

    refactor: create module with all const variables

---
+++ modules/helpers/constVariables.js
@@ -0,0 +1,15 @@
+export const codeOfLastSmallLetter = 122;
+export const codeOfFirstSmallLetter = 97;
+export const codeBeforeTheFirstSmallLetter = 96;
+export const encrypt = 'ENCRYPT';
+export const discrypt = 'DISCRYPT';
+export const cipherCasear = 'Casear';
+export const cipherROT_8 = 'ROT-8';
+export const cipherAtbash = 'Atbash';
+export const INPUT = ['-i', '--input'];
+export const OUTPUT = ['-o', '--output'];
+export const CONFIG = ['-c', '--config'];
+export const regExString = new RegExp('[a-z]', 'gi');
+export const regExOptions = new RegExp('^-{1,2}[ioc]', 'g');
+export const cipherCasearStep = 1;
+export const cipherROT_8_Step = 8;
