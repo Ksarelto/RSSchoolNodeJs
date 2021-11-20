ee30e94f07dcbfeedca242cfe665e63957c450e9
Artsiom Murashko <ksarelto@tut.by>
2021-11-20 16:29:39 +0300

    refactor: update streamsPipe func

--- modules/streams/streamsPipe.js
+++ modules/streams/streamsPipe.js
@@ -1,16 +1,18 @@
 import process from 'process';
 import { transformString } from '../createTransformStreams.js';
-import { pipeline } from 'stream';
+import { pipeline } from 'stream/promises';
 import { myWritableStream, myReadableStream } from './streams.js';
 import { setErrorsMessage } from '../helpers/utils.js';
 
-export const streamsPipe = (input, output, command) => {
-  const readableStream = input ? new myReadableStream(input) : process.stdin;
-  const writableStream = output ? new myWritableStream(output) : process.stdout;
-  const transformStreams = transformString(command);
-  pipeline(readableStream, ...transformStreams, writableStream, (err) => {
-    if (err) {
-      setErrorsMessage(err);
-    }
-  });
+export const streamsPipe = async (input, output, command) => {
+  try {
+    const readableStream = input ? new myReadableStream(input) : process.stdin;
+    const writableStream = output
+      ? new myWritableStream(output)
+      : process.stdout;
+    const transformStreams = transformString(command);
+    await pipeline(readableStream, ...transformStreams, writableStream);
+  } catch (err) {
+    setErrorsMessage(err);
+  }
 };
