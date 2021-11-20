123898fec2fab6a6a838fbe11e7d58f60b21bec0
Artsiom Murashko <ksarelto@tut.by>
2021-11-20 17:18:48 +0300

    feat: add config correct tests

---
+++ __test__/configCorrect.test.js
@@ -0,0 +1,65 @@
+import { spawn } from 'child_process';
+import fs from 'fs';
+
+const spawnCorrectProcess = (command) => {
+  return new Promise((resolve, reject) => {
+    const args = command.split(' ');
+    const childProcess = spawn('node', [...args], { stdio: 'pipe' });
+
+    childProcess.on('close', () => {
+      fs.readFile('output.txt', 'utf-8', (err, data) => {
+        if (err) reject(new Error('Test failed'));
+        resolve(data);
+      });
+    });
+
+    childProcess.on('exit', () => {
+      childProcess.kill();
+    });
+  });
+};
+
+const responseStrings = {
+  'C1-C1-R0-A': 'Myxn xn nbdobm. Tbnnfzb ferlm "_" nhteru!',
+  'C1-C0-A-R1-R0-A-R0-R0-C1-A': 'Vhgw gw wkmxkv. Ckwwoik onauv "_" wqcnad!',
+  'A-A-A-R1-R0-R0-R0-C1-C1-A': 'Hvwg wg gsqfsh. Asggous opcih "_" gmapcz!',
+  'C1-R1-C0-C0-A-R0-R1-R1-A-C1': 'This is secret. Message about "_" symbol!',
+};
+
+describe('Correct tests of CLI', () => {
+  beforeEach(() => {
+    try {
+      fs.writeFileSync('output.txt', '', { encoding: 'utf-8', flag: 'r' });
+    } catch (err) {
+      console.log(err.message);
+    }
+  });
+
+  test('should show correct output with args C1-C1-R0-A -i', async () => {
+    const response = await spawnCorrectProcess(
+      'index.js -c C1-C1-R0-A -i ./input.txt -o ./output.txt'
+    );
+    expect(response).toMatch(responseStrings['C1-C1-R0-A']);
+  });
+
+  test('should show correct output with args C1-C0-A-R1-R0-A-R0-R0-C1-A', async () => {
+    const response = await spawnCorrectProcess(
+      'index.js -c C1-C0-A-R1-R0-A-R0-R0-C1-A -i ./input.txt -o ./output.txt'
+    );
+    expect(response).toMatch(responseStrings['C1-C0-A-R1-R0-A-R0-R0-C1-A']);
+  });
+
+  test('should show correct output with args A-A-A-R1-R0-R0-R0-C1-C1-A', async () => {
+    const response = await spawnCorrectProcess(
+      'index.js -c A-A-A-R1-R0-R0-R0-C1-C1-A -i ./input.txt -o ./output.txt'
+    );
+    expect(response).toMatch(responseStrings['A-A-A-R1-R0-R0-R0-C1-C1-A']);
+  });
+
+  test('should show correct output with args C1-R1-C0-C0-A-R0-R1-R1-A-C1', async () => {
+    const response = await spawnCorrectProcess(
+      'index.js -c C1-R1-C0-C0-A-R0-R1-R1-A-C1 -i ./input.txt -o ./output.txt'
+    );
+    expect(response).toMatch(responseStrings['C1-R1-C0-C0-A-R0-R1-R1-A-C1']);
+  });
+});
