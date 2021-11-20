605f1ddbbf18b3b4270bc169cc07811c521f5256
Artsiom Murashko <ksarelto@tut.by>
2021-11-20 23:51:20 +0300

    feat: add additional test for transform Object

--- __test__/transformStreamObject.test.js
+++ __test__/transformStreamObject.test.js
@@ -1,22 +1,20 @@
 import { jest } from '@jest/globals';
 import fs from 'fs';
 import { pipeline } from 'stream/promises';
+import { cipherCasearEncoding } from '../modules/ciphersFuncs';
 import { cipherCasear, encrypt } from '../modules/helpers/constVariables';
 import { myTransformStream } from '../modules/streams/streams';
 
-const errorMessage = jest.fn(() => {
+const testFunc = jest.fn();
+testFunc.mockImplementation(() => {
   throw new Error('Test Error');
 });
 
-const imitatePipeline = async () => {
+const imitatePipeline = async (func, cipher = cipherCasear, mode = encrypt) => {
   try {
     const read = fs.createReadStream('input.txt');
     const write = fs.createWriteStream('output.txt');
-    const transform = new myTransformStream(
-      errorMessage,
-      cipherCasear,
-      encrypt
-    );
+    const transform = new myTransformStream(func, cipher, mode);
     await pipeline(read, transform, write);
   } catch (err) {
     return err.message;
@@ -25,7 +23,23 @@ const imitatePipeline = async () => {
 
 describe('test transform stream', () => {
   test('should throw exeption', async () => {
-    const res = await imitatePipeline();
+    testFunc.mockImplementation(() => {
+      throw new Error('Test Error');
+    });
+    const res = await imitatePipeline(testFunc);
     expect(res).toBe('Test Error');
+  });
+  test('should return correct answer', async () => {
+    testFunc.mockImplementation(cipherCasearEncoding);
+    const res = await imitatePipeline(testFunc);
+    expect(res).toBeUndefined();
+  });
+  test('should return not default string', async () => {
+    fs.writeFileSync('./output.txt', '', { encoding: 'utf-8', flag: 'r' });
+    testFunc.mockImplementation(cipherCasearEncoding);
+    const res = await imitatePipeline(testFunc, 'Incorrect Cipher', 'WTF');
+    expect(res).toBeUndefined();
+    const result = fs.readFileSync('./output.txt', 'utf-8');
+    expect(result).not.toBe('This is secret. Message about "_" symbol!');
   });
 });
